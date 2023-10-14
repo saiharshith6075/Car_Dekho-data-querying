0421a6ea231b279d09500fffe2749a9f92cae79a
Sai Harshith <114991015+saiharshith6075@users.noreply.github.com>
2023-10-14 20:28:47 +0530

    Update car_dekho_script_file.sql

--- car_dekho_script_file.sql
+++ car_dekho_script_file.sql
@@ -1,14 +1,14 @@
-								#(Car Dekho) SQL Assignment Project 1
-							#Second Hand Car Dealer Database Questions And Answers 
+								#(Car Dekho) SQL Assignment Project-1
+							#Second Hand Car Dealer Database Problems 
 
-											-- PROBLEM  STATEMENTS
+								  -- PROBLEM  STATEMENTS
 #	Manager's Query to Employee: How many cars are expected to be available in the year 2023?
-SELECT COUNT(*) FROM car_dekho WHERE year=2023; #6 
+SELECT COUNT(*) FROM car_dekho WHERE year=2023;  
 
 #	Manager's Query to Employee: Could you provide the number of cars available in the years 2020, 2021, and 2022?
 SELECT year,COUNT(*) FROM car_dekho WHERE year IN(2020,2021,2022) GROUP BY year;
 
-#	Client's Request to Employee: Please share the total count of cars, categorized by year.
+#	Client's Request to Employee: Please share the total count of cars, categorized by year?
 SELECT year,COUNT(*) FROM car_dekho GROUP BY year;
 
 #	Client's Inquiry to Car Dealer: How many diesel cars were available in the year 2020, and can you provide this information?
@@ -40,7 +40,7 @@ SELECT name, COUNT(*) AS top_count FROM car_dekho GROUP BY name ORDER BY top_cou
 SELECT name,selling_price FROM car_dekho WHERE year=2023 ORDER BY selling_price DESC LIMIT 10;
 
 #	Manager's Query to Employee: Breakdown of car transmissions and their respective counts.
-	SELECT transmission,COUNT(*) FROM car_dekho GROUP BY transmission;
+SELECT transmission,COUNT(*) FROM car_dekho GROUP BY transmission;
 
 #	Client's Query to Car Dealer: Please provide the count of cars for each number of seats in 2022.
 SELECT seats,COUNT(*) AS car_count FROM car_dekho WHERE year=2022 GROUP BY seats;
